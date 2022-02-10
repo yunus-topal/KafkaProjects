1e7c270e53dbd9f1a079cdf8c98237dcf1366fc5
Yunus Emre Topal <emretopal20@gmail.com>
2022-02-10 22:06:38 +0300

    simple consumer added

---
+++ KafkaConsumer/src/main/java/Main.java
@@ -0,0 +1,56 @@
+import java.nio.charset.Charset;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Random random = new Random();
+        char[] str = new char[10];
+        for(int i = 0; i<10; i++){
+            str[i] = (char) random.nextInt(50,100);
+        }
+        System.out.println(str);
+        String id = String.valueOf(str);
+        singleConsumerGroup(id);
+    }
+
+    public static void singleConsumer(String groupId) throws InterruptedException {
+        SampleConsumer sampleConsumer = new SampleConsumer("Consumer", groupId);
+    }
+
+    public static void singleConsumerGroup(String groupId) throws InterruptedException {
+        Thread t1 = new Thread() {
+            @Override
+            public void run() {
+                SampleConsumer sampleConsumer1 = new SampleConsumer("Consumer1", groupId);
+            }
+        };
+
+        Thread t2 = new Thread() {
+            @Override
+            public void run() {
+                SampleConsumer sampleConsumer2 = new SampleConsumer("Consumer2", groupId);
+            }
+        };
+
+
+        Thread t3 = new Thread() {
+            @Override
+            public void run() {
+                SampleConsumer sampleConsumer3 = new SampleConsumer("Consumer3", groupId);
+            }
+        };
+
+        Thread t4 = new Thread() {
+            @Override
+            public void run() {
+                SampleConsumer sampleConsumer4 = new SampleConsumer("Consumer4", groupId);
+            }
+        };
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+
+    }
+}
---
+++ KafkaConsumer/src/main/java/SampleConsumer.java
@@ -0,0 +1,31 @@
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class SampleConsumer {
+    private String name;
+    public SampleConsumer(String name, String id){
+        this.name = name;
+        Properties props = new Properties();
+        props.setProperty("bootstrap.servers", "localhost:9092");
+        props.setProperty("group.id", id);
+        props.setProperty("enable.auto.commit", "true");
+        props.setProperty("auto.commit.interval.ms", "1000");
+        props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("consumer_group"));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record : records)
+                System.out.printf("consumer: %s offset = %d, key = %s, value = %s%n", this.name, record.offset(), record.key(), record.value());
+        }
+    }
+}
